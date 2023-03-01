bca215d7141cb11e5c6e99a2239874c41a919d10
Phil Crissman <phil.crissman@gmail.com>
2023-02-28 16:15:24 -0800

    reverse-string in cpp

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,13 @@
 #include "reverse_string.h"
+#include <algorithm>
 
 namespace reverse_string {
+
+std::string reverse_string(std::string in_string) {
+    std::string reversed = "";
+    for (std::string::reverse_iterator rev_it=in_string.rbegin(); rev_it!=in_string.rend(); ++rev_it)
+        reversed += *rev_it;
+    return reversed;
+}
 
 }  // namespace reverse_string
