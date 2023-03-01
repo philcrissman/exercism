60756bcc5af5e58f3d1708a14ef34d44e3808aac
Phil Crissman <phil.crissman@gmail.com>
2023-02-28 20:18:55 -0800

    fixed / finished space-age

--- cpp/space-age/space_age.cpp
+++ cpp/space-age/space_age.cpp
@@ -1,13 +1,51 @@
 #include "space_age.h"
 
-namespace space_age {
-    space_age::space_age(const long &seconds) : age_in_seconds(seconds)
-    {
-    }
+space_age::space_age::space_age(long long seconds) noexcept
+: age_in_seconds(seconds)
+{
+}
 
-    long space_age::seconds()
-    {
-        return age_in_seconds;
-    }
+long long space_age::space_age::seconds() const noexcept
+{
+    return age_in_seconds;
+}
 
-}  // namespace space_age
+double space_age::space_age::on_earth() const noexcept
+{
+    return age_in_seconds / 60.0 / 60.0 / 24.0 / 365.25;
+}
+
+double space_age::space_age::on_mercury() const noexcept
+{
+    return on_earth() / 0.2408467;
+}
+
+double space_age::space_age::on_venus() const noexcept
+{
+    return on_earth() / 0.61519726;
+}
+
+double space_age::space_age::on_mars() const noexcept
+{
+    return on_earth() / 1.8808158;
+}
+
+double space_age::space_age::on_jupiter() const noexcept
+{
+    return on_earth() / 11.862615;
+}
+
+double space_age::space_age::on_saturn() const noexcept
+{
+    return on_earth() / 29.447498;
+}
+
+double space_age::space_age::on_uranus() const noexcept
+{
+    return on_earth() / 84.016846;
+}
+
+double space_age::space_age::on_neptune() const noexcept
+{
+    return on_earth() / 164.79132;
+}
--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -1,13 +1,28 @@
-#pragma once
+#ifndef SPACE_AGE_H
+#define SPACE_AGE_H
 
-namespace space_age {
+namespace space_age 
+{
 
-    class space_age {
-        private:
-            long age_in_seconds;
-
+    class space_age 
+    {
         public:
-            space_age(const long& seconds);
-            long seconds();
+            space_age(long long seconds) noexcept;
+            long long seconds() const noexcept;
+            double on_earth() const noexcept;
+            double on_mercury() const noexcept;
+            double on_venus() const noexcept;
+            double on_mars() const noexcept;
+            double on_jupiter() const noexcept;
+            double on_saturn() const noexcept;
+            double on_uranus() const noexcept;
+            double on_neptune() const noexcept;
+
+        private:
+            long long age_in_seconds;
+
     };
 }  // namespace space_age
+
+
+#endif
