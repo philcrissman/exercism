3456ad46ac5cebecba85a99e95480e8637c7c3ac
Phil Crissman <phil.crissman@gmail.com>
2023-03-10 07:12:20 -0800

    Finished raindrops, the boring way

--- cpp/raindrops/raindrops.cpp
+++ cpp/raindrops/raindrops.cpp
@@ -1,5 +1,25 @@
 #include "raindrops.h"
 
+#include <cmath>
+#include <iostream>
+
 namespace raindrops {
+    std::string convert(long n)
+    {
+        std::string resp = "";
+        if (n % 3 == 0) {
+            resp += "Pling";
+        }
+        if (n % 5 == 0){
+            resp += "Plang";
+        }
+        if (n % 7 == 0){
+            resp += "Plong";
+        }
+        if (resp.empty()) {
+            resp = std::to_string(n);
+        }
+        return resp;
+    }
 
 }  // namespace raindrops
