f0ed3c0d4b6994bb6181b2bc8c4f2f1a15228023
Phil Crissman <phil.crissman@gmail.com>
2023-11-08 13:46:08 -0800

    Update grains.cpp
    change name of function; `square` sounds like it is returning the square of n, and it is not.

--- cpp/grains/grains.cpp
+++ cpp/grains/grains.cpp
@@ -6,7 +6,7 @@
 #include <numeric>
 
 namespace grains {
-    unsigned long long square(int i)
+    unsigned long long grains_on_square(int i)
     {
         return std::pow(2, i-1);
     }
@@ -14,7 +14,7 @@ namespace grains {
     unsigned long long total() {
         std::vector<unsigned long long> i(64);
         std::iota(i.begin(), i.end(), 1);
-        std::transform(i.begin(), i.end(), i.begin(), [](int n){ return square(n); });
+        std::transform(i.begin(), i.end(), i.begin(), [](int n){ return grains_on_square(n); });
         return std::accumulate(i.begin(), i.end(), 0);
     }
 
